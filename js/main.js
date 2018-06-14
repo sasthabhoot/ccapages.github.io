b187a46b05dd46456c21d8474673d1a55c1536a4
Soumya Ranjan Mondal <letter2srm@gmail.com>
2018-06-14 11:58:38 +0530

    inception

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+TweenMax.to("#arrow",0.5,{
+  top: 130,
+  delay:1,
+  repeat: 5,
+  yoyo: true
+});
+
+
+$(document).ready( () => {
+
+  $('.menu-button').on('click', () => {
+    $('.menu-list').toggleClass('hide');
+  })
+
+})
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
